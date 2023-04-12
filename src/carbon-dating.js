c9f2d746a76ae49361e8f8cea91f6715a6add61d
nivarnas <rpankratoff@gmail.com>
2023-04-12 19:16:28 +0300

    carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string"){
+    return false
+  }
+
+  if (! Boolean(Number(sampleActivity))) {
+    return false
+  }
+
+  if (Number(sampleActivity) < 0 || Number(sampleActivity) > 15) {
+    return false
+  }
+
+
+  if (Math.ceil((HALF_LIFE_PERIOD / Math.log(2)) * (Math.log(MODERN_ACTIVITY / Number(sampleActivity)))) < 0) {
+    return false
+  }  else {return Math.ceil((HALF_LIFE_PERIOD / Math.log(2)) * (Math.log(MODERN_ACTIVITY / Number(sampleActivity)))) }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
