4c5bf39e0e5f23d2e1f716c656f9a32343d70d00
nivarnas <rpankratoff@gmail.com>
2023-04-13 18:49:40 +0300

    extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  console.log(options)
+  let basicOptions = {
+    separator: '+',
+    addition: '',
+    // separator: '',
+    additionSeparator: '|',
+    additionRepeatTimes: 1,
+    repeatTimes: 1
+  }
+
+  for (key in basicOptions) {
+    // options[key] = options[key].toString();
+    if (! options[key] && (options[key] !== false) && (options[key] !== null)) {
+      options[key] = basicOptions[key];
+    }
+    // if (options[key])
+
+    if (options[key] === null) {
+      options[key] = 'null'
+    } else {
+      // options[key] = options[key].toString();
+      options[key] += '';
+    }
+  }
+  console.log('             ' + str)
+  // console.log(options);
+
+  // if (! options.separator) {
+  //   options.separator = basicOptions.separator
+  // }
+
+  // if (! options.additionSeparator) {
+  //   options.additionSeparator = basicOptions.additionSeparator;    
+  // }
+
+  // for (key in options) {
+  //   options[key] = options[key].toString();
+  // }
+
+  let result = ''
+
+  // result = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition + options.separator).repeat(options.repeatTimes - 1) +
+  // (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition)
+  
+  let additions = ''
+
+  if (options.additionRepeatTimes) {
+    additions = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition
+  }
+
+  // console.log(additions)
+
+  result = (str + additions + options.separator).repeat(options.repeatTimes - 1) + (str + additions)
+
+  return result
+
+  // console.log(options);
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
