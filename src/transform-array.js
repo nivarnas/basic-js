ddfe5d4ff5dfd93aaa7b41e26efe6e280282df19
nivarnas <rpankratoff@gmail.com>
2023-12-13 18:35:23 +0300

    feat:a add transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,9 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  if (Array.isArray(arr)) {
-    throw new Error ('"arr" parameter must be an instance of the Array!')
+  if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!")
   }
+
+  // if(arr.length === 0) {
+  //   return arr;
+  // }
+
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (result.length !== 0 && arr[i - 2] !== '--discard-next') result.pop()
+    } else if (arr[i] === '--double-next') {
+      if (arr[i + 1] ) result.push(arr[i + 1])
+    } else if (arr[i] === '--double-prev') {
+      if (arr[i - 1] && arr[i - 2] !== '--discard-next') result.push(arr[i - 1])
+    } else {
+      result.push(arr[i])
+    }
+
+   }
+   return result
 }
 
 module.exports = {
